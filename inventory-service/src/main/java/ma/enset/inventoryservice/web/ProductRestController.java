3060b69eab7232375030d4872bc6faf544ca39d1
AbdelilahGlsid <abdelilah.bouhkka-etu@etu.univh2c.ma>
2024-12-15 23:12:27 +0100

    part 1

---
+++ inventory-service/src/main/java/ma/enset/inventoryservice/web/ProductRestController.java
@@ -0,0 +1,35 @@
+package ma.enset.inventoryservice.web;
+
+import ma.enset.inventoryservice.entities.Product;
+import ma.enset.inventoryservice.repositories.ProductRepository;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class ProductRestController {
+    private ProductRepository productRepository;
+
+    public ProductRestController(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @GetMapping("/products")
+    //@PreAuthorize("hasAuthority('ADMIN')")
+    public List<Product> productList(){
+        return productRepository.findAll();
+    }
+
+    @GetMapping("/products/{id}")
+    //@PreAuthorize("hasAuthority('USER')")
+    public Product productById(@PathVariable String id){
+        return productRepository.findById(id).get();
+    }
+
+    @GetMapping("/auth")
+    public Authentication authentication(Authentication authentication){
+        return authentication;
+    }
+}
